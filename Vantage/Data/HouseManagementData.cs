dac7ce45a310c6d71ff324979a7e19dcf15b0e5a
Jake Magill <solidmagill@gmail.com>
2015-09-14 17:38:00 -0700

    Added model and dummy data for duties

--- Vantage/Data/HouseManagementData.cs
+++ Vantage/Data/HouseManagementData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Vantage.Models;
 
 namespace Vantage.Data
 {
@@ -18,6 +19,58 @@ namespace Vantage.Data
                 "Derek Sucks 4",
                 "Derek Sucks 5"
             };
-        } 
+        }
+
+        public static List<DutyModel> GetDuties()
+        {
+            var duties = new List<DutyModel>();
+
+            for (var i = 0; i < 25; i++)
+            {
+                DutyStatus dutyStatus;
+                if (i == 0)
+                {
+                    dutyStatus = DutyStatus.MajorFail;
+                }
+                else if (i == 1)
+                {
+                    dutyStatus = DutyStatus.MinorFail;
+                }
+                else if (i == 1)
+                {
+                    dutyStatus = DutyStatus.Incomplete;
+                }
+                else if (i == 1)
+                {
+                    dutyStatus = DutyStatus.Complete;
+                }
+                else 
+                {
+                    dutyStatus = DutyStatus.Passed;
+                }
+
+                duties.Add(new DutyModel()
+                {
+                    Id = i,
+                    Title = "Duty Title " + i,
+                    Description = "This is a description of how to do this duty. There should be a decent amount of text here so I'm going to go ahead and make this string pretty long",
+                    Performance = GenerateDutyPerformance(dutyStatus)
+                });
+            }
+
+            return duties;
+        }
+
+        private static DutyStatus[] GenerateDutyPerformance(DutyStatus status)
+        {
+            var output = new DutyStatus[7];
+
+            for (var i = 0; i < 7; i++)
+            {
+                output[i] = status;
+            }
+
+            return output;
+        }
     }
 }
---
+++ Vantage/Models/DutyModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vantage.Models
+{
+    public class DutyModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DutyStatus[] Performance { get; set; }
+    }
+
+    public enum DutyStatus
+    {
+        Complete=1,
+        MinorFail=-1,
+        MajorFail=-2,
+        Passed=2,
+        Incomplete=0
+    }
+}
