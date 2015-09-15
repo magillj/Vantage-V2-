0135c31a604a5cb975d775bad7601ad3f50d7cdd
Derek Coley <dmcoley@cs.washington.edu>
2015-09-15 15:59:28 -0700

    Modal added

--- Vantage/Scripts/common.js
+++ Vantage/Scripts/common.js
@@ -1,1 +1,7 @@
 ﻿/* Common functions used by multiple javascript pages */
+
+function showCommonModal(title, body) {
+    $("#commonModalTitle").html(title);
+    $("#commonModalBody").html(body);
+    $("#commonModal").modal("show");
+}
