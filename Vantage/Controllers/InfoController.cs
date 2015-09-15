9f50ad3cabd16ca9adb01738f693d0265da6aad4
Derek Coley <dmcoley@cs.washington.edu>
2015-09-15 16:37:07 -0700

    404 :(

---
+++ Vantage/Controllers/InfoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vantage.Data;
+using Vantage.Models;
+
+namespace Vantage.Controllers
+{
+    public class InfoController : Controller
+    {
+
+        // GET: Info or Info/Index
+        public ActionResult Index()
+        {
+            // TODO: Make a home page for Information
+            //return View("InformationHome");
+            return new HttpStatusCodeResult(400, "Page not created yet");
+        }
+
+        // GET: Information/Directory
+        public ActionResult Directory()
+        {
+            DirectoryViewModel viewModel = new DirectoryViewModel()
+            {
+                TestVar = "Hello, world!",
+            };
+            return View("Directory", viewModel);
+        }
+    }
+}
---
+++ Vantage/Models/DirectoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Vantage.Models
+{
+    public class DirectoryViewModel
+    {
+        // TODO: load with information needed on the directory
+        public string TestVar { get; set; }
+    }
+}
